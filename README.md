aa8ed00223a479d4a5c7fd0cb75ea1ba010434aa
Andy <66146370+andosgee@users.noreply.github.com>
2024-05-02 22:15:48 +1200

    Finished Marquee Implementation
    Marquee Implementation with sample media. Media is not final product and is simply a placeholder.

--- README.md
+++ README.md
@@ -3,14 +3,20 @@
 ## Langauges/Markup Used
 PHP, HTML, JS, SASS, SQL
 
-## Run On
+## Runs On
 Xampp Stack, using PHP MyAdmin, MYsql, Apache
 
-### Use Case
+## Use Case
 Skill Forge is a full stack development that simplifies SOP training and Staff training tools through a Web-based application. It will allow multiple types of users (admins, clients) to interact through a log in to their own companies resources.
 It allows for the company to create a module that can be completed by the user for training and/or compliance. Its aim is to centralise a learning hub in a business enviroment.
 
-### Useful Login information
+## Installation Instructions
+
+## Index Page
+### Marquee
+Uses x amount of picture from the assets/image/assets/marquee/ directory. Each image lasts for 5 seconds and can be overrun from the side buttons or using the radio buttons at the bottom of the marquee.
+
+## Useful Login information
 **Company Name** \
 **Company Admin** - *Username*: <> *Password*: <> \
 **Company User** - *Username*: <> *Password*: <> \
--- includes/marquee.php
+++ includes/marquee.php
@@ -1,11 +1,25 @@
 <div class="marquee">
-    <div class="marquee__content">
-        <img src="assets/image/assets/marquee/image1.jpg" alt="Image 1">
-        <img src="assets/image/assets/marquee/image2.jpg" alt="Image 2">
-        <img src="assets/image/assets/marquee/image3.jpg" alt="Image 3">
-        <img src="assets/image/assets/marquee/image4.jpg" alt="Image 4">
-        <img src="assets/image/assets/marquee/image5.jpg" alt="Image 5">
-        <img src="assets/image/assets/marquee/image6.jpg" alt="Image 6">
+    <div class="marquee__wrapper">
+        <?php 
+            $images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg"];
+
+            foreach ($images as $image) {
+                echo '<img src="assets/image/assets/marquee/'. $image .'">';
+            }
+        ?>
     </div>
+    <div class="marquee__controls">
+            <button class="button__prev">&#10094;</button>
+            <button class="button__next">&#10095;</button>
+    </div>
+    <div class="marquee__selector">
+            <?php
+            // Output thumbnails or dots for each slide
+            foreach ($images as $index => $image) {
+                echo '<div class="marquee__selector__image" data-index="assets/image/assets/marquee/' . $index . '"></div>';
+            }
+            ?>
+        </div>
+    
 </div>
 <script src="js/marquee.js"></script>
