d9b6b45eec180abf44afa9251dc53664024725d7
Andy <66146370+andosgee@users.noreply.github.com>
2024-05-06 22:35:44 +1200

    Change Header based on Page

--- includes/headernav.php
+++ includes/headernav.php
@@ -1,7 +1,18 @@
 <header class="header">
     <img class="header__logo" src="assets/image/logo/SkillForge.svg" alt="Skillforge Logo">
     <h1 class="header__h1">SKILLFORGE | Crafting Skills & Forging Success</h1>
-    <a class="header__link" href="login.php">Login</a>
+    <?php 
+        $pageName = getPageTitle();
+        if($pageName == "Home") {
+            echo"<a class='header__link' href='login.php'>Login</a>";
+        }elseif($pageName =="Login" or $pageName == "Staff Login") {
+            echo "<a class='header__link' href='index.php'>Home</a>";
+        // }elseif($pageName =="Staff Login"){
+        //     echo "<a class='header__link' href='index.php'>Home</a>";
+        }else{
+            echo "<a class='header__link' href='logout.php'>Logout</a>";
+        }
+    ?>
 </header>
 <nav class="navigation">
     <a href="#about" class="navigation__link">About Us</a>
