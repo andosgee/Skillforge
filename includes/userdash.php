13269172ea1834fb7428a13744588103411679ef
Andy <66146370+andosgee@users.noreply.github.com>
2024-05-14 17:22:46 +1200

    Standard and Admin User Dashboard
    Creation of the dashboard for admins of companies and standard users

--- includes/userdash.php
+++ includes/userdash.php
@@ -4,26 +4,76 @@
     ?>
     <div class="content_wrapper">
         <div class="content__tiles">
-            <div class="content__tiles__item">
-                My Details
-                <br>
+            <a href="./sample.php" class="content__tiles__item">
+                <h2>My Details</h2>
                 Modify your details here.
-            </div>
-            <div class="content__tiles__item">
-                My Module History
-                <br>
+                <p></p>
+                <img class="content__tiles__item__image" src="./assets/image/logo/mydetails.svg" alt="My details icon.">
+            </a>
+            <a href="./sample.php" class="content__tiles__item">
+               <h2>My Module History</h2>
                 View your completed modules here.
-            </div>
-            <div class="content__tiles__item">
-                Required Modules
-                <br>
+                <p></p>
+                <img class="content__tiles__item__image" src="./assets/image/logo/modulehistory.svg" alt="Completed modules icon.">
+            </a>
+            <a href="./sample.php" class="content__tiles__item">
+                <h2>Required Modules</h2>
                 You have X modules that need to be completed.
-            </div>
-            <div class="content__tiles__item">
-                Module Search
-                <br>
+                <p></p>
+                <img class="content__tiles__item__image" src="./assets/image/logo/requiredmodules.svg" alt="Required Modules icon.">
+            </a>
+            <a href="./sample.php" class="content__tiles__item">
+                <h2>Module Search</h2>
                 Search for a Module to complete here.
-            </div>
+                <p></p>
+                <img class="content__tiles__item__image" src="./assets/image/logo/searchmodules.svg" alt="Module Search icon.">
+            </a>
+            <?php 
+                $sqlGetAdmin = "SELECT ul.levelName
+                FROM userdata ud
+                INNER JOIN userlevel ul ON ud.level = ul.levelID
+                WHERE ud.userID = '{$_SESSION['userID']}'
+                AND ud.companyID = ul.companyID";          
+                $result = $conn -> query( $sqlGetAdmin );
+                if ($result && $result->num_rows > 0) {
+                    // Fetch the result row
+                    $row = $result->fetch_assoc();
+                    // Extract the levelName
+                    $levelName = $row['levelName'];
+                    if ($levelName == 'Admin') {
+                        echo "<a href='./sample.php' class='content__tiles__item'>
+                        <h2>Reports</h2>
+                        Generate Reports.
+                        <p></p>
+                        <img class='content__tiles__item__image' src='./assets/image/logo/generatereports.svg' alt='Reports icon.'>
+                    </a>
+                    <a href='./sample.php' class='content__tiles__item'>
+                        <h2>Edit Module</h2>
+                        Select a Module to Edit.
+                        <p></p>
+                        <img class='content__tiles__item__image' src='./assets/image/logo/editmodules.svg' alt='Edit Modules icon.'>
+                    </a>
+                    <a href='./sample.php' class='content__tiles__item'>
+                        <h2>Assign Module</h2>
+                        Select a Module to Assign.
+                        <p></p>
+                        <img class='content__tiles__item__image' src='./assets/image/logo/assignmodules.svg' alt='Assign Modules icon.'>
+                    </a>
+                    <a href='./sample.php' class='content__tiles__item'>
+                        <h2>Create Module</h2>
+                        Create a module.
+                        <p></p>
+                        <img class='content__tiles__item__image' src='./assets/image/logo/searchmodules.svg' alt='Create Modules icon.'>
+                    </a>
+                    <a href='./sample.php' class='content__tiles__item'>
+                        <h2>Manage Users</h2>
+                        Create and manage users.
+                        <p></p>
+                        <img class='content__tiles__item__image' src='./assets/image/logo/manageusers.svg' alt='Manage Users icon.'>
+                    </a>";
+                    }
+                }
+            ?>
         </div>
     </div>
 </div>
