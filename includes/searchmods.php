1bbf2538a94f9a67f7b76f47aeefe7d93b3b6514
Andy <66146370+andosgee@users.noreply.github.com>
2024-06-13 13:30:14 +1200

    Module Search Bar

--- includes/searchmods.php
+++ includes/searchmods.php
@@ -1,5 +1,29 @@
 <div clas="content">
+    <div class="content__search">
+        <form class="form">
+            <label for="search" class="form__label">Search:</label>
+            <input type="text" class="form__search" id="search" placeholder="Search">
+            <label for="sort" class="form__label">Sort By:</label>
+            <select name="sort" id="sort" class="form__select">
+                <option>Sort Type</option>
+                <option>Name Asc</option>
+                <option>Name Dsc</option>
+                <option>Published Asc</option>
+                <option>Published Dsc</option>
+            </select>
+            <label for="filter" class="form__label">Filter:</label>
+            <select name="filter" id="filter" class="form__select">
+                <option>Filter Type</option>
+                <option>Required</option>
+                <option>Compliance</option>
+                <option>Skills</option>
+                <option>Optional</option>
+            </select>
+            <input name="form__submitInput" type="submit" value="Search/Filter" class="form__submitInput">
+        </form>
+    </div>
     <div class="content__wrapper">
+        
         <div class="content__tiles">
             <a href="./run_module.php#SiteInduction" class="content__tiles__item">
                 <img src="./assets/image/assets/sampleModules/induction.png" alt="Site Induction Image" class="content__tiles__item__image">
