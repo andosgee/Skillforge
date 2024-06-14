1d1073de0d98621a21418e75c7c9fb19a0c9e8db
Andy <66146370+andosgee@users.noreply.github.com>
2024-06-14 13:28:33 +1200

    CC License

--- includes/footer.php
+++ includes/footer.php
@@ -25,7 +25,7 @@
             <a class="footer__link" href="./staff_login.php">Login Here</a>
         </div>
         <div class="footer__text">
-            Something Needs to go here in final Eval
+        <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><span property="dct:title">Skillforge</span> by <span property="cc:attributionName">Andrew Grant</span> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>
         </div>
         <div class="footer__text">
             <h3>Useful Information</h3>
